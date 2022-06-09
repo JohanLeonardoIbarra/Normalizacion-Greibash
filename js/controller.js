ffb63f8a76a10c379b7e76ca508dffa1a960146d
@johanleonardois <johanleonardois@ufps.edu.co>
2022-06-08 21:37:59 -0500

    Interfaz terminada pendiente por revision de errores

--- js/controller.js
+++ js/controller.js
@@ -27,20 +27,24 @@ formulario.addEventListener("submit", (event) => {
     alert("Las variables terminales deben ser numeros separados por comas");
     return;
   }
-  document.getElementById("first-view").classList.add("run-left");
-  document.getElementById("modal").classList.toggle("hide");
-  document.getElementById("modal").classList.add("show");
+  const main = document.getElementById("first-view");
+  main.classList.remove("animate__fadeIn");
+  main.classList.add("animate__fadeOutLeft");
+
+  const prods = document.getElementById("modal");
+  prods.classList.toggle("hide");
+  prods.classList.add("animate__fadeInRight");
 
   alfa = alfabeto.split(",");
   init = inicial;
   terminal = terminales.split(",");
 
-  document.body.classList.add("two");
+  document.getElementById("imgbg").classList.remove("one");
+  document.getElementById("imgbg").classList.add("two");
   agregarProducciones();
 });
 
 const formProd = document.getElementById("formulario-producciones");
-
 formProd.addEventListener("submit", (event) => {
   event.preventDefault();
 });
@@ -62,23 +66,41 @@ const agregarProducciones = () => {
     const producciones = [...Array(count).keys()].map((x) => {
       return data.get("prod" + x);
     });
-    document.getElementById("modal").classList.toggle("run-left2");
+    document.getElementById("first-view").classList.add("hide");
 
-    document.getElementById("view").classList.toggle("hide");
-    document.getElementById("view").classList.add("show2");
+    const prods = document.getElementById("modal");
+    prods.classList.remove("animate__fadeInRight");
+    prods.classList.add("animate__fadeOutLeft");
+    setTimeout(() => {
+      prods.classList.add("hide");
+    }, 1000);
+
+    const vista = document.getElementById("view");
+    vista.classList.toggle("hide");
+    vista.classList.add("animate__fadeInRight");
+
+    document.getElementById("imgbg").classList.remove("two");
+    document.getElementById("imgbg").classList.add("three");
+
     prod = producciones;
     app();
   });
 };
 
-const graficar = (resultados) => {
+const graficar = (resultados, descripcion, titulo) => {
   const results = document.getElementById("results");
-  results.innerHTML += template(resultados);
+  const div = document.createElement("div");
+  div.setAttribute(
+    "class",
+    "w-[80%] border rounded-lg border-blue-400 my-6 animate__animated animate__flipInX"
+  );
+  div.innerHTML = template(resultados, descripcion, titulo);
+  results.appendChild(div);
 };
 
 const resTemplate = (data) => {
   return `
-  <tr>
+  <tr">
     <td>${data[0]}</td>
     <td>${data[1]}</td>
   <tr/>
@@ -90,7 +112,7 @@ const template = (data, desc, title) => {
   <h1 class="text-5xl">${title}</h1>
   <p class="text-xl">${desc}</p>
 </div>
-<table class="border border-solid border-blue-400 w-[80%] fade">
+<table class="border border-solid border-blue-400 w-full">
   <thead>
     <tr>
       <th>Variable No Terminal</th>
@@ -112,28 +134,52 @@ const app = () => {
     model = new Modelo(alfa, init, prod, terminal);
     model.buscarNulas();
     c++;
-    graficar(model.crearMatriz());
+    graficar(
+      model.crearMatriz(),
+      "Variables y sus producciones previas a iniciar el proceso de normalizacion siguiendo paso a paso hata llegar a la forma normal de greibach",
+      "Gramatica"
+    );
   } else if (c === 1) {
     model.eliminarInutiles();
     c++;
-    graficar(model.crearMatriz());
+    graficar(
+      model.crearMatriz(),
+      "Eliminacion de variables presentes en las producciones que no se encuentran en la gramatica",
+      "Eliminacion de Producciones Inutiles"
+    );
   } else if (c === 2) {
     model.eliminarInalcanzables();
     c++;
-    graficar(model.crearMatriz());
+    graficar(
+      model.crearMatriz(),
+      "Eliminacion de variables que son inalcanzables desde la variable inicial del sistema",
+      "Elminacion de Variables Inalcanzables"
+    );
   } else if (c === 3) {
     model.eliminarNulos();
     c++;
-    graficar(model.crearMatriz());
+    graficar(
+      model.crearMatriz(),
+      "Eliminacion de producciones que contienen elementos nulos junto con su respetivo ajuste en otras produccines.",
+      "Eliminacion de Producciones Nulas"
+    );
   } else if (c === 4) {
     model.eliminarUnitarias();
     model.eliminarUndefined();
     c++;
-    graficar(model.crearMatriz());
+    graficar(
+      model.crearMatriz(),
+      "Eliminacion de producciones unitarias remplazando las por el valor de sus producciones",
+      "Eliminacion de Producciones Unitarias"
+    );
   } else if (c === 5) {
     model.chomsky();
     c++;
-    graficar(model.crearMatriz());
+    graficar(
+      model.crearMatriz(),
+      "Normalizacion a la forma normal de chomsky generando variables con producciones de maximo 2 variables no terminales",
+      "Forma Normal de Chomsky"
+    );
   }
 };
 
